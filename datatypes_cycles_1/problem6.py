d49783e6a5a78f557f60bf4ae89c709ac8704043
Mikhail Kharlamov <e.mikhailov@itigris.ru>
2021-04-19 00:48:08 +0300

    Small fix

--- datatypes_cycles_1/problem6.py
+++ datatypes_cycles_1/problem6.py
@@ -2,9 +2,7 @@ lst = map(int, input().split())
 cnts = {}
 
 for l in lst:
-    if l not in cnts:
-        cnts[l] = 1
-    else:
-        cnts[l] += 1
+    cnts.setdefault(l, 0)
+    cnts[l] += 1
 
 print(*sorted([k for k, v in cnts.items() if v > 1]))
